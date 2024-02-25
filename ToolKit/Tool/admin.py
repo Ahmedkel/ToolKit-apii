8e93113e36df838799c8ce6b82974a28b619ea54
Ahmedkel <ahmedkeloch@gmail.com>
2024-02-25 18:47:51 +0100

    Add docstring for Display_listOfTools class in admin.py

--- ToolKit/Tool/admin.py
+++ ToolKit/Tool/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import website_Tools
 
 class Display_listOfTools(admin.ModelAdmin):
+    """ Display the list of tools in the admin panel. """
     list_display = ['name', 'pricing', 'description', 'category', 'url', 'created_at', 'updated_at']
     search_fields = ['name', 'pricing', 'category']
     
