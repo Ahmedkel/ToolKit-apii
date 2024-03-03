ea74e9926706b22188e0a0f1243b9df79f795fff
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-03 16:06:10 +0100

    Refactor URLs in ToolKit/Tool/urls.py

--- ToolKit/Tool/urls.py
+++ ToolKit/Tool/urls.py
@@ -1,15 +1,10 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-
 urlpatterns = [
-    re_path('signup', views.signup),
-    re_path('login', views.login),
-    re_path('test_token', views.test_token),
-    path('', views.website_ToolsList, name='website_tools'),
-    path('website_tools/', views.website_ToolsList, name='website_tools'),
-    path('website_tools/<int:id>/', views.website_ToolsDetail, name='website_tools_detail'),
+    path('website_tools/', views.website_ToolsList.as_view(), name='website_tools'),
+    path('website_tools/<int:id>/', views.website_ToolsDetail.as_view(), name='website_tools_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
