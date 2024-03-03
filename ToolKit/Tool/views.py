0c7533bdcc5bc478271d8da917738860a6b12649
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-03 16:22:40 +0100

    Refactor views.py file

--- ToolKit/Tool/views.py
+++ ToolKit/Tool/views.py
@@ -1,52 +1,17 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
-from django.http import JsonResponse
 from .models import website_Tools,Category
-from .serializers import website_ToolsSerializer, UserSerializer
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from .serializers import website_ToolsSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser
 
 
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 
 from rest_framework import generics
-from rest_framework.generics import RetrieveDestroyAPIView
 from .models import website_Tools, Category
 from .serializers import website_ToolsSerializer
 from rest_framework.permissions import IsAdminUser
 
-# @api_view(['GET', 'POST'])
-# @permission_classes([IsAdminUser])
-# def website_ToolsList(request, format=None):
-#     """
-#     This function is used to list all the website
-#     tools and also to add a new website tool.
-#     """
-#     if request.method == 'GET':
-#         website_tools = website_Tools.objects.all()
-#         serializer = website_ToolsSerializer(website_tools, many=True)
-#         return Response(serializer.data)
-
-#     if request.method == 'POST':
-#         # Check if the category exists, if not create it
-#         category_name = request.data.get('category_name')
-#         category, created = Category.objects.get_or_create(name=category_name)
-
-#         # Update the request data with the category instance
-#         mutable = request.POST._mutable
-#         request.POST._mutable = True
-#         request.data['category'] = category.id
-#         request.POST._mutable = mutable
-        
-#         serializer = website_ToolsSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
 
 class website_ToolsList(generics.ListCreateAPIView):
     """
@@ -67,33 +32,6 @@ class website_ToolsList(generics.ListCreateAPIView):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# @api_view(['GET', 'PUT', 'DELETE'])
-# @permission_classes([IsAdminUser])
-# def website_ToolsDetail(request, id, format=None):
-#     """
-#     This function is used to retrieve a single
-#     website tool by its id.
-#     """
-#     try:
-#         website_tool = website_Tools.objects.get(id=id)
-#     except website_Tools.DoesNotExist:
-#         return Response(status=404)
-    
-#     if request.method == 'GET':
-#         serializer = website_ToolsSerializer(website_tool)
-#         return Response(serializer.data)
-    
-#     elif request.method == 'PUT':
-#         serializer = website_ToolsSerializer(website_tool, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=400)
-    
-#     elif request.method == 'DELETE':
-#         website_tool.delete()
-#         return Response(status=204)
 
 class website_ToolsDetail(generics.RetrieveUpdateDestroyAPIView):
     """
