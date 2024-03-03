03611a62f59a3b3c65e26b5ebf5851fe7c880574
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-03 16:04:21 +0100

    Add category filter to list of tools in admin panel

--- ToolKit/Tool/admin.py
+++ ToolKit/Tool/admin.py
@@ -5,6 +5,7 @@ class Display_listOfTools(admin.ModelAdmin):
     """ Display the list of tools in the admin panel. """
     list_display = ['name', 'pricing', 'description', 'category', 'url', 'created_at', 'updated_at']
     search_fields = ['name', 'pricing', 'category']
+    list_filter = ('category',)
     
 
 admin.site.register(website_Tools, Display_listOfTools)
