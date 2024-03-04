b3e8acdd748a5dc35117152751247d1fe7c73e80
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-04 18:30:45 +0100

    Update unique constraints and field lengths in website_Tools model

--- ToolKit/Tool/models.py
+++ ToolKit/Tool/models.py
@@ -14,14 +14,14 @@ class Category(models.Model):
 class website_Tools(models.Model):
     """ This class is used to create the website_Tools model. """
     id = models.AutoField(primary_key=True, unique=True, editable=False)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(unique=True, max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     pricing = models.CharField(max_length=8, choices=PricingChoices.choices, default=PricingChoices.FREE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     # image = models.ImageField(upload_to='images/')
-    url = models.URLField()
+    url = models.URLField(unique=True)
     
     def __str__(self):
         return self.name + ' - ' + self.created_at.strftime('%d-%m-%Y %H:%M:%S')
