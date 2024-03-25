56146976ba62d67a475aca569f6ce2652e82060e
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-25 17:01:05 +0000

    Remove commented out code in urls.py

--- ToolKit/Tool/urls.py
+++ ToolKit/Tool/urls.py
@@ -4,7 +4,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('website_tools/', views.Website_ToolsList.as_view(), name='website_tools'),
-    # path('', views.website_ToolsList.as_view(), name='website_tools'),
     path('', views.home, name='home'),
     path('website_tools/<int:id>/', views.Website_ToolsDetail.as_view(), name='website_tools_detail'),
     path('website_tools/<str:category_name>/', views.ToolsByCategoryList.as_view(), name='tools_by_category'),
