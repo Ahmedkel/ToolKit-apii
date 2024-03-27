eadf1210f4fb431a5fb12d14edd40d29e02732f9
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-27 18:06:00 +0000

    Refactor Website_ToolsSerializer in serializers.py removing unused code and adding new img field

--- ToolKit/Tool/serializers.py
+++ ToolKit/Tool/serializers.py
@@ -11,26 +11,9 @@ class Website_ToolsSerializer(serializers.ModelSerializer):
         required=True
     )
 
-# old 
-# class website_ToolsSerializer(serializers.ModelSerializer):
-#     """ This class is used to serialize the website_Tools model. """
-#     category_name = serializers.CharField(source='category.name')
-
     class Meta:
         model = Website_Tools
-        fields = ['id', 'name', 'category', 'description', 'created_at', 'updated_at', 'pricing', 'url']
-    
-    # class Meta:
-    #     model = website_Tools
-    #     fields = ['id', 'name', 'category_name', 'description', 'created_at', 'updated_at', 'pricing', 'url']
-    
-    # old create method    
-    # def create(self, validated_data):
-    #     """ Create a new website_tool object with the validated data."""
-    #     category_name = validated_data.pop('category').name
-    #     category, created = Category.objects.get_or_create(name=category_name)
-    #     website_tool = website_Tools.objects.create(category=category, **validated_data)
-    #     return website_tool
+        fields = ['id', 'name', 'category', 'image', 'description', 'created_at', 'updated_at', 'pricing', 'url']
     
     def create(self, validated_data):
         """ Create a new website_tool object with the validated data."""
@@ -38,13 +21,6 @@ class Website_ToolsSerializer(serializers.ModelSerializer):
         category, created = Category.objects.get_or_create(name=category_name)
         validated_data['category'] = category
         return super().create(validated_data)
-
-    # old validate method
-    # def validate_category(self, value):
-    #     """ Custom validation for the category field. """
-    #     if not Category.objects.filter(name=value).exists():
-    #         raise serializers.ValidationError("Invalid category name.")
-    #     return value
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(object):
