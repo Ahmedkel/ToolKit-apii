5f76bf15940e34b7f28975a88e89a35c49f8a035
Ahmedkel <ahmedkeloch@gmail.com>
2024-03-27 18:04:58 +0000

    Refactor URLs to include API versioning

--- ToolKit/Tool/urls.py
+++ ToolKit/Tool/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('website_tools/', views.Website_ToolsList.as_view(), name='website_tools'),
     path('', views.home, name='home'),
-    path('website_tools/<int:id>/', views.Website_ToolsDetail.as_view(), name='website_tools_detail'),
-    path('website_tools/<str:category_name>/', views.ToolsByCategoryList.as_view(), name='tools_by_category'),
-]
+    path('api/v1/',
+         include([
+                path('website_tools/', views.Website_ToolsList.as_view(), name='website_tools'),
+                path('website_tools/<int:id>/', views.Website_ToolsDetail.as_view(), name='website_tools_detail'),
+                path('website_tools/<str:category_name>/', views.ToolsByCategoryList.as_view(), name='tools_by_category'),
+         ])
+        ),
+    ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
