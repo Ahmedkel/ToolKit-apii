0e1290941a816745ab8ae365c11b4d9b94082126
Ahmedkel <ahmedkeloch@gmail.com>
2024-04-23 15:57:52 +0100

    Refactor URLs in urls.py to handle media files in DEBUG mode

--- ToolKit/ToolKit/urls.py
+++ ToolKit/ToolKit/urls.py
@@ -22,4 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('7lwa/', admin.site.urls),
     path('', include('Tool.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
